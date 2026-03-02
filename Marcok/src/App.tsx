77de617e28907acdd320999222fa836797f030ce
DevZenthor <zenthorpro@outlook.com>
2026-03-02 20:24:56 +0100

    feat : add section collaboration

--- Marcok/src/App.tsx
+++ Marcok/src/App.tsx
@@ -3,6 +3,10 @@ import Marcok from "./assets/marcok.png"
 import FLC from "./assets/FLC.svg"
 import TwoR from "./assets/2R.svg"
 import OneP from "./assets/1P.svg"
+import Dreyqan from "./assets/dreyqan.jpg"
+import Baki from "./assets/baki.jpg"
+import Zuki from "./assets/zuki.jpg"
+import Novac from "./assets/novax.jpg"
 import { FaYoutube, FaTwitch, FaTiktok, FaTwitter } from "react-icons/fa"
 
 function App() {
@@ -16,6 +20,7 @@ function App() {
           <ul className="nav-links">
             <li><a href="#hero">Accueil</a></li>
             <li><a href="#reseaux">Réseaux</a></li>
+            <li><a href="#collab">Collab</a></li>
             <li><a href="#carriere">Carrière</a></li>
             <li><a href="#live">Live</a></li>
           </ul>
@@ -55,6 +60,39 @@ function App() {
           <a href="https://x.com/marcokttv" className="card" target="_blank" rel="noopener noreferrer">
             <FaTwitter size={40}/> Twitter
           </a>
+        </div>
+      </section>
+
+      {/* COLLAB */}
+      <section id="collab" className="collab">
+        <h2>Mes Collaborations</h2>
+
+        <div className="collab-cards">
+
+          <a href="https://x.com/dreyqan" target="_blank" rel="noopener noreferrer" className="collab-card">
+            <img src={Dreyqan} alt="Dreyqan" />
+            <h3>Dreyqan</h3>
+            <span className="status">F/A</span>
+          </a>
+
+          <a href="https://x.com/coach_baki" target="_blank" rel="noopener noreferrer" className="collab-card">
+            <img src={Baki} alt="Baki" />
+            <h3>Baki</h3>
+            <span className="status">F/A</span>
+          </a>
+
+          <a href="https://x.com/VZuki_1" target="_blank" rel="noopener noreferrer" className="collab-card">
+            <img src={Zuki} alt="Zuki" />
+            <h3>Zuki</h3>
+            <span className="status">F/A</span>
+          </a>
+
+          <a href="https://x.com/novax7q" target="_blank" rel="noopener noreferrer" className="collab-card">
+            <img src={Novac} alt="Novac" />
+            <h3>Novac</h3>
+            <span className="status">F/A</span>
+          </a>
+
         </div>
       </section>
 
